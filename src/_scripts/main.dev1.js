4c17299d6e18933bcdf8bb463fcd15a692359ec2
vishalsahody <vishalsahody@gmail.com>
2021-09-13 12:05:32 +1000

    Avoid conflict multi dev

---
+++ src/_scripts/main.dev1.js
@@ -0,0 +1,11 @@
+// Main javascript entry point
+
+'use strict';
+
+window.$ = jQuery;
+// import { Link } from '../_modules/link/link';
+
+$(() => {
+//   new Link(); // Activate Link modules logic
+  console.log('Dev1');
+});
